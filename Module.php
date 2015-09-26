8e40af6ceee9dd500c5eaabf9d06672823ffd899
Serge Larin <serge.larin@gmail.com>
2015-09-26 17:58:43 +0300

    Added standard module class

---
+++ Module.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Sitemap module
+ *
+ * @author Serge Larin <serge.larin@gmail.com>
+ * @link http://assayer.pro/
+ * @copyright 2015 Assayer Pro Company
+ * @license http://opensource.org/licenses/LGPL-3.0
+ */
+
+
+namespace assayerpro\sitemap;
+
+/**
+ * Class Module
+ *
+ * @package app\modules\webmaster
+ */
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'assayerpro\sitemap\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
