a76a92cde6b8984576c25136f9ab4df683f28dcc
Serge Larin <serge.larin@gmail.com>
2015-10-01 09:20:06 +0300

    Added console command for sitemap generate

---
+++ console/CreateController.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * CreateController for sitemap module
+ *
+ * @link https://github.com/assayer-pro/yii2-sitemap-module
+ * @author Serge Larin <serge.larin@gmail.com>
+ * @copyright 2015 Assayer Pro Company
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace assayerpro\sitemap\console;
+
+use Yii;
+use yii\console\Controller;
+use yii\helpers\Console;
+
+
+/**
+ * Generate sitemap for application
+ *
+ * @author Serge Larin <serge.larin@gmail.com>
+ * @package assayerpro\sitemap
+ */
+class CreateController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public $defaultAction = 'create';
+    /**
+     * Generate sitemap.xml file
+     *
+     * @access public
+     * @return string
+     */
+    public function actionCreate($file='@webroot/sitemap.xml')
+    {
+        $file = Yii::getAlias($file);
+        $this->stdout("Generate sitemap file.\n", Console::FG_GREEN);
+        $this->stdout("Rendering sitemap...\n", Console::FG_GREEN);
+        $sitemap = Yii::$app->sitemap->render();
+        $this->stdout("Writing sitemap to $file\n", Console::FG_GREEN);
+        file_put_contents($file, $sitemap);
+        $this->stdout("Done\n", Console::FG_GREEN);
+        return self::EXIT_CODE_NORMAL;
+    }
+}
