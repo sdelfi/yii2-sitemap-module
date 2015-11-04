b8d584a0c7ba4606098485767ca4b3b2905dc7f6
Serge Larin <serge.larin@gmail.com>
2015-11-04 10:55:41 +0300

    Added tests for RobotsTxt class

---
+++ tests/unit/RobotsTxtTest.php
@@ -0,0 +1,25 @@
+<?php
+use \assayerpro\sitemap\RobotsTxt;
+
+
+class RobotsTxtTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testRobotstxt()
+    {
+    }
+
+}
