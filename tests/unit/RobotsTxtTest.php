0b27c252de9d40cd1a02137a58a9cd6b49af92d2
Serge Larin <serge.larin@gmail.com>
2015-11-04 11:01:27 +0300

    Added more tests for RobotsTxt

--- tests/unit/RobotsTxtTest.php
+++ tests/unit/RobotsTxtTest.php
@@ -18,8 +18,30 @@ class RobotsTxtTest extends \Codeception\TestCase\Test
     }
 
     // tests
-    public function testRobotstxt()
+    public function testRobotsTxtHost()
     {
+        $robotstxt = new RobotsTxt();
+        $this->assertEquals('www.example.com', $robotstxt->host);
+        $this->assertEquals("Host: www.example.com\n", $robotstxt->render());
+    }
+    public function testRobotsTxtHostHttps()
+    {
+        $_SERVER['HTTPS'] = 'on';
+        $robotstxt = new RobotsTxt();
+        $this->assertEquals('https://www.example.com', $robotstxt->host);
+        $this->assertEquals("Host: https://www.example.com\n", $robotstxt->render());
+    }
+    public function testRobotsTxtSitemap()
+    {
+        $robotstxt = new RobotsTxt();
+        $robotstxt->sitemap = 'http://www.example.com/sitemap.xml';
+        $this->assertEquals("Host: www.example.com\nSitemap: http://www.example.com/sitemap.xml\n", $robotstxt->render());
+    }
+    public function testRobotsTxtCreateWithParams()
+    {
+        $_SERVER['HTTPS'] = 'on';
+        $robotstxt = new RobotsTxt(['host' => 'example.net', 'sitemap' => 'http://example.net/data/sitemap.xml']);
+        $this->assertEquals("Host: example.net\nSitemap: http://example.net/data/sitemap.xml\n", $robotstxt->render());
     }
 
 }
