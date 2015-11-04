2e24dc372592852baa92b53b90dd243ea867adaf
Serge Larin <serge.larin@gmail.com>
2015-11-04 14:39:24 +0300

    Added unit test for RobotsTxt->userAgent

--- tests/unit/RobotsTxtTest.php
+++ tests/unit/RobotsTxtTest.php
@@ -43,5 +43,45 @@ class RobotsTxtTest extends \Codeception\TestCase\Test
         $robotstxt = new RobotsTxt(['host' => 'example.net', 'sitemap' => 'http://example.net/data/sitemap.xml']);
         $this->assertEquals("Host: example.net\nSitemap: http://example.net/data/sitemap.xml\n", $robotstxt->render());
     }
+    public function testRobotsTxtUseragent()
+    {
+        $robotstxt = new RobotsTxt([
+            'userAgent' => [
+                '*' => [
+                    'Disallow' => [
+                        ['/main/default/index'],
+                        '/api',
+                        ['/api/version2/index'],
+                    ],
+                    'Allow' => [
+                        '/api/doc',
+                    ]
+                ],
+                'Googlebot-Image' => [
+                    'Disallow' => [
+                        '/img/',
+                        '/*.gif$',
+                    ]
+                ]
+            ]
+        ]);
+        $expectedRobotsTxt = <<<EOF
+Host: www.example.com
+User-agent: *
+Disallow: /
+Disallow: /api
+Disallow: /api-v2
+Allow: /api/doc
+User-agent: Googlebot-Image
+Disallow: /img/
+Disallow: /*.gif$
+
+EOF;
+        $this->assertEquals($expectedRobotsTxt, $robotstxt->render());
+        $robotstxt = new RobotsTxt([
+            'userAgent' => []
+        ]);
+        $this->assertEquals("Host: www.example.com\n", $robotstxt->render());
+    }
 
 }
